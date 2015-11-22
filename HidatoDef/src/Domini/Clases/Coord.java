b6da81ee0f7487a584c146e83741987417f4a57a
Carlo <Carlo@192.168.1.37>
2015-11-22 21:19:44 +0100

    Cambios menores packages, ya estan authors

--- HidatoDef/src/Domini/Clases/Coord.java
+++ HidatoDef/src/Domini/Clases/Coord.java
@@ -1,5 +1,8 @@
 package Domini.Clases;
-
+/**
+ *
+ * @author Carlos
+ */
 public class Coord {
 	public int x;
 	public int y;
--- HidatoDef/src/Domini/Controlador/ControladorSoluciona.java
+++ HidatoDef/src/Domini/Controlador/ControladorSoluciona.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 /**
  *
- * @author Carlo
+ * @author Carlos
  */
 public class ControladorSoluciona {
     
