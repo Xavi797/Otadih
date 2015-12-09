e1c30b252f49eb35ee739afd4d324114ea83dfdf
jaume.guell <jaume.guell@c6s302pc52.fib.upc.es>
2015-12-09 12:10:51 +0100

    Problemes amb el ordenador de ranking. Controlador Persistencia iniciat. He posat un package més (Persistencia.Controlador), OJO AL COMPILAR!

---
+++ HidatoDef/src/Persistencia/Controlador/ControladorPersistencia.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistencia.Controlador;
+
+import Persistencia.SerialitzadorEstadistiques;
+import Persistencia.SerialitzadorPartides;
+import Persistencia.SerialitzadorRankings;
+import Persistencia.SerialitzadorTaulers;
+import Persistencia.SerialitzadorUsuaris;
+
+/**
+ *
+ * @author jaume.guell
+ */
+public class ControladorPersistencia {
+    
+    /* Serialitzadors encarregats de fer les operacions*/
+    private SerialitzadorEstadistiques SEstadistiques;
+    private SerialitzadorPartides SPartides;
+    private SerialitzadorRankings SRankings;
+    private SerialitzadorTaulers STaulers;
+    private SerialitzadorUsuaris SUsuaris;
+    
+    //Comprovar si usuari-password existeix
+    
+    //Comprovar si usuari-codi existeix
+    
+    //Guardat i carregat de tot (COMPROVAR SI ES RETORNA UN OBJECTE BUIT!!)
+}
