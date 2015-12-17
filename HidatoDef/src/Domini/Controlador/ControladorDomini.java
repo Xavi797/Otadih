10ba3d19ec0aa615b25459067a5212e44d5a28ca
jaumeguellsabate <jaumeguellsabate@192.168.1.132>
2015-12-18 00:33:03 +0100

    Puntuacions amb checks feta.

--- HidatoDef/src/Domini/Controlador/ControladorDomini.java
+++ HidatoDef/src/Domini/Controlador/ControladorDomini.java
@@ -100,6 +100,7 @@ public class ControladorDomini {
              * Despres fara els sets y getters per tenir aqui el tauler.
              */
             public void generaTauler(int costat, int numInicials, String topo){
+                partida = new Partida();
                 cGen.generaTauler(costat, numInicials ,topo);
                 tauler = cGen.getTauler();
                 tauler_partida = tauler.clonar();
@@ -611,6 +612,7 @@ public class ControladorDomini {
              */
             public void augmentaChecks() {
                 partida.setNumChecks(partida.getNumChecks() + 1);
+                System.out.println(partida.getNumChecks());
             }
             
             /**** TAULER ****/
